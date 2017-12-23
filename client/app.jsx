affbef647dc3b035d35bfc8bbb7b78ec6077186c
Jerome Dane <jerome.dane@gmail.com>
2017-12-23 16:10:52 -0500

    Fix code style
    Signed-off-by: Jerome Dane <jerome.dane@gmail.com>

--- client/app.jsx
+++ client/app.jsx
@@ -7,13 +7,13 @@ export default class extends Component {
         <h1>Hello, World!</h1>
         {series &&
           <ul>
-            {series.map(({name, countOfIssues, startYear, image: {thumbUrl}, publisher}) =>
+            {series.map(({name, countOfIssues, startYear, image: {thumbUrl}, publisher}) => (
               <li style="clear: left;">
-                <img src={thumbUrl} style="float: left; margin: 0 1em 1em 0;"/>
+                <img src={thumbUrl} style="float: left; margin: 0 1em 1em 0;" />
                 {name} ({startYear}) - {countOfIssues} issue{countOfIssues === 1 ? '' : 's'}
                 <div>Published by {publisher.name}</div>
               </li>
-            )}
+            ))}
           </ul>
         }
       </div>
